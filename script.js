9279ffe38334c6bb0eb1321702cc48ed279530e5
Ryan Rotman <ryan.rotman@gmail.com>
2020-08-15 23:52:43 -0500

    updated content

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ var choiceCBtn = document.querySelector("#choiceC");
 var choiceDBtn = document.querySelector("#choiceD");
 var secondsLeft = 75;
 var currentIndex = 0;
+var timerInterval = 0;
+var initials = "";
+var listOfHighscores = []
 
 // Array with multiple objects to hold all quiz questions and answers
 var questionArr = [{
@@ -21,29 +24,29 @@ var questionArr = [{
     correctAnswer: "C"
 },
 {
-    question: "Question 2",
-    answers: ["a: this is answer a", "b: this is answer b", "c: this is answer c", "d: this is answer d"],
-    correctAnswer: ""
+    question: "What symbol is used for the termination in JavaScript syntax?",
+    answers: ["a: &gt;", "b: &quot", "c: &semi;", "d: &amp"],
+    correctAnswer: "C"
 },
 {
-    question: "Question 3",
-    answers: ["a: this is answer a", "b: this is answer b", "c: this is answer c", "d: this is answer d"],
-    correctAnswer: ""
+    question: "What element is NOT a part of JavaScript?",
+    answers: ["a: Variable", "b: h1", "c: Array", "d: Object"],
+    correctAnswer: "B"
 },
 {
-    question: "Question 4",
-    answers: ["a: this is answer a", "b: this is answer b", "c: this is answer c", "d: this is answer d"],
-    correctAnswer: ""
+    question: "Which element is useful for debugging your code?",
+    answers: ["a: console.log", "b: function", "c: event listener", "d: click event"],
+    correctAnswer: "A"
 },
 {
-    question: "Question 5",
-    answers: ["a: this is answer a", "b: this is answer b", "c: this is answer c", "d: this is answer d"],
-    correctAnswer: ""
+    question: "Which element does NOT creat a popup in your browser?",
+    answers: ["a: Alert", "b: Confirm", "c: Prompt", "d: console.log"],
+    correctAnswer: "D"
 }];
 
 // Set the timer
 function setTime() {
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.innerHTML = secondsLeft + " seconds left";
         // console.log(secondsLeft);
@@ -59,74 +62,61 @@ function setTime() {
 
 // Set the questions and answer choices for the quiz
 function setQuestion() {
-    quizQuestion.textContent = questionArr[currentIndex].question;
-    choiceABtn.innerHTML = questionArr[currentIndex].answers[0];
-    choiceBBtn.innerHTML = questionArr[currentIndex].answers[1];
-    choiceCBtn.innerHTML = questionArr[currentIndex].answers[2];
-    choiceDBtn.innerHTML = questionArr[currentIndex].answers[3];
-}
+    if (currentIndex <= 4) {
+        quizQuestion.textContent = questionArr[currentIndex].question;
+        choiceABtn.innerHTML = questionArr[currentIndex].answers[0];
+        choiceBBtn.innerHTML = questionArr[currentIndex].answers[1];
+        choiceCBtn.innerHTML = questionArr[currentIndex].answers[2];
+        choiceDBtn.innerHTML = questionArr[currentIndex].answers[3];
+    } else {
+        clearInterval(timerInterval);
+        initials = prompt("Please input your initials to register your score.")
+        logInitials();
+    }
+};
 
 // Get and check the user's answer againt the correct answer from array
 function checkAnswer(event) {
-    var userClicked = event.target.dataset.value;
+    console.log(event);
+    var userClicked = event.target.dataset.value
     console.log("User clicked: " + userClicked);
+    if (userClicked === questionArr[currentIndex].correctAnswer) {
+        currentIndex++;
+        setQuestion();
+    } else {
+        secondsLeft = secondsLeft - 10;
+        currentIndex++;
+        setQuestion();
+    }
+};
+
+// Log the initials and score into local storage
+function logInitials() {
+    localStorage.setItem("initials", initials);
+    localStorage.setItem("score", secondsLeft);
 }
 
-// Set quiz questions and answers
-    // How is this set up? Arrays?
-        // One main array with multiple objects
-        //var questionArr =[
-            //{question: "abcd"
-            //choices:["a","b","c","d"]
-            //answer:"a"
-            //},
-            //{object2}
-        //]
-            // each object will contain the question, the choices, and the answer
-    // All done on one page, so show/hide properties?
-        // use styling to display: none and display: block
-        // create buttons dynamically using javascript by looping through the array
-            // var currentIndex = 0 global var
-            // questionArr[0].question
-            // questionArr[0].choices.forEach() or using another for loop
-
-        //when user clicks the answer: currentIndex++, compare answer to choices, decrease time, scoring.
-        //when time <= 0: game is over, move to initial container
-    // Need text field for question and buttons for answers
-
-// Show first question when button is clicked (along with starting the timer)
-    // Make necessary adjustments to setTime() as needed
-
-// Show next question when answer is chosen for prior question
-
-// Compare user inputed answer and correct answer
-    // If user clicked = correct answer then continue to next question
-
-// If answer selected is wrong, take away time from the timer
-    // Else subtract __ seconds from secondsLeft and continue to next question
-
-// When all questions are answered or timer reaches 0, allow for saving name and score
-    // Is score based on points or time?
-    // localStorage.setItem()
-
-// View highscores page
+// TODO:
+    // When all questions are answered or timer reaches 0, allow for saving name and score
+        // function storeHighscores() {
+        // }
+    // View highscores page
     // localStorage.getItem()
     // Add button to return back to the start of the quiz
     // Add button to clear all highscores
-        // Highscores saved in local storage
-        // localStorage.clear()
-
-// Will have multiple buttons all with .addEventListener("click") to them
+    // Highscores saved in local storage
+    // localStorage.clear()
 
 // Add event listener to the button to begin the quiz as well as the timer
 beginQuizBtn.addEventListener("click", function () {
     setTime();
     beginQuiz.setAttribute("style", "display: none");
-    quizSection.setAttribute("style","display: block !important");
+    quizSection.setAttribute("style", "display: block !important");
     setQuestion();
 });
+
 // Add event listener to each answer choice button
-choiceABtn.addEventListener("click", checkAnswer());
-choiceBBtn.addEventListener("click", checkAnswer());
-choiceCBtn.addEventListener("click", checkAnswer());
-choiceDBtn.addEventListener("click", checkAnswer());
+choiceABtn.addEventListener("click", checkAnswer);
+choiceBBtn.addEventListener("click", checkAnswer);
+choiceCBtn.addEventListener("click", checkAnswer);
+choiceDBtn.addEventListener("click", checkAnswer);
